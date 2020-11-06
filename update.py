23350ccc39274e803778dcff234ea056354c093e
Adam McMaster <adam.mcmaster@open.ac.uk>
2020-11-06 15:51:39 +0000

    Subject saving optimisation
    - Go back to reloading all of them
    - Turn on async saves
    - Infer from metadata if subject has been processed

--- update.py
+++ update.py
@@ -16,12 +16,6 @@ PROCESSED_SETS_FILE = 'processed_sets.txt'
 with open('config.yaml') as config_f:
     config = yaml.load(config_f, Loader=yaml.FullLoader)
 
-if os.path.isfile(PROCESSED_SUBJECTS_FILE):
-    with open(PROCESSED_SUBJECTS_FILE) as processed_f:
-        processed_subjects = { s.strip() for s in processed_f.readlines() }
-else:
-    processed_subjects = set()
-
 if os.path.isfile(PROCESSED_SETS_FILE):
     with open(PROCESSED_SETS_FILE) as processed_f:
         processed_sets = { s.strip() for s in processed_f.readlines() }
@@ -31,19 +25,19 @@ else:
 Panoptes.connect(**config)
 project = Project(PROJECT_ID)
 
-with open(PROCESSED_SUBJECTS_FILE, 'a') as processed_subjects_f:
-    with open(PROCESSED_SETS_FILE, 'a') as processed_sets_f:
-        for subject_set in project.links.subject_sets:
-            if subject_set.id in processed_sets:
-                continue
-            with ChargingBar(
-                'Updating {}'.format(subject_set.display_name),
-                max=subject_set.set_member_subjects_count,
-                suffix='%(percent).1f%% %(eta_td)s'
-            ) as bar:
+with open(PROCESSED_SETS_FILE, 'a') as processed_sets_f:
+    for subject_set in project.links.subject_sets:
+        if subject_set.id in processed_sets:
+            continue
+        with ChargingBar(
+            'Updating {}'.format(subject_set.display_name),
+            max=subject_set.set_member_subjects_count,
+            suffix='%(percent).1f%% %(eta_td)s'
+        ) as bar:
+            with Subject.async_saves():
                 for subject in Subject.where(subject_set_id=subject_set.id, page_size=100):
                     bar.next()
-                    if subject.id in processed_subjects:
+                    if '!CERiT' in subject.metadata:
                         continue
 
                     superwasp_id = subject.metadata.get('Filename', subject.metadata.get('filename')).split('_')[0]
@@ -64,24 +58,16 @@ with open(PROCESSED_SUBJECTS_FILE, 'a') as processed_subjects_f:
                     simbad_url = 'http://simbad.u-strasbg.fr/simbad/sim-coo?Coord={}+{}&Radius=2&Radius.unit=arcmin&submit=submit+query'.format(ra, dec)
                     asassn_url = 'https://asas-sn.osu.edu/photometry?ra={}&dec={}&radius=2'.format(ra, dec)
 
-                    for retry in (True, False):
-                        try:
-                            subject.metadata['!CERiT'] = cerit_url
-                            subject.metadata['!Simbad'] = simbad_url
-                            subject.metadata['!ASAS-SN Photometry'] = asassn_url
+                    # Reload the subject to refresh the etag
+                    # This works around what seems to be a bug in the Panoptes client
+                    # https://github.com/zooniverse/panoptes-python-client/issues/220
+                    subject.reload()
+                    subject.metadata['!CERiT'] = cerit_url
+                    subject.metadata['!Simbad'] = simbad_url
+                    subject.metadata['!ASAS-SN Photometry'] = asassn_url
 
-                            subject.save()
-                        except PanoptesAPIException:
-                            if retry:
-                                # Reload the subject to refresh the etag
-                                # This works around what seems to be a bug in the Panoptes client
-                                # https://github.com/zooniverse/panoptes-python-client/issues/220
-                                subject.reload()
-                            else:
-                                raise
-
-                    processed_subjects.add(subject.id)
-                    processed_subjects_f.write('{}\n'.format(subject.id))
+                    subject.save()
+        
                 bar.finish()
             processed_sets.add(subject_set.id)
             processed_sets_f.write('{}\n'.format(subject_set.id))
