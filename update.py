340ea36eb2dad35cfbba9c1cbea55e99cfd78b99
Adam McMaster <adam.mcmaster@open.ac.uk>
2020-11-06 10:36:16 +0000

    Reload subject before modifying

--- update.py
+++ update.py
@@ -57,6 +57,10 @@ with open(PROCESSED_SUBJECTS_FILE, 'a') as processed_subjects_f:
                 simbad_url = 'http://simbad.u-strasbg.fr/simbad/sim-coo?Coord={}+{}&Radius=2&Radius.unit=arcmin&submit=submit+query'.format(ra, dec)
                 asassn_url = 'https://asas-sn.osu.edu/photometry?ra={}&dec={}&radius=2'.format(ra, dec)
 
+                # Reload the subject to refresh the etag
+                # This works around what seems to be a bug in the Panoptes client
+                # https://github.com/zooniverse/panoptes-python-client/issues/220
+                subject.reload()
                 subject.metadata['!CERiT'] = cerit_url
                 subject.metadata['!Simbad'] = simbad_url
                 subject.metadata['!ASAS-SN Photometry'] = asassn_url
